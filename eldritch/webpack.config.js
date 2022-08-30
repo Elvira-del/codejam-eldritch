13ddc847ac3995b63af45455ef5327f352fc99b3
Elvira-del <ninelostr@gmail.com>
2022-08-30 17:12:58 +0300

    refactor: change webpack config

--- eldritch/webpack.config.js
+++ eldritch/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'asset/[name][ext]',
     clean: true,
